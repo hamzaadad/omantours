b31a75c95ac9410cb896049392694c563a94dec9
hamzaadad <adadhamza@outlook.fr>
2016-06-28 15:18:27 +0200

    town lang tempalte

--- www/js/controllers/TownController.js
+++ www/js/controllers/TownController.js
@@ -5,6 +5,46 @@ app
   $scope.go = function(state){
     $state.go(state);
   }
+
+
+  $scope.formateLange = function(langdata){
+     $scope.welcom = langdata.data.town.welcom;
+     $scope.lang = langdata.data.town;
+
+   }
+
+   $scope.changelanguage = function(name){
+     $ionicLoading.hide();
+     $ionicLoading.show();
+     ApiFactory.getLanguage(name).then(function(resp){
+       LocalFactory.setLanguage(resp.data);
+       $scope.formateLange(resp.data[0]);
+       $ionicLoading.hide();
+     }, function(err){
+       console.log(err);
+     })
+   }
+
+   if(!LocalFactory.getLanguage()){
+     $scope.changelanguage("eng");
+   }else{
+     $scope.formateLange(LocalFactory.getLanguage()[0]);
+     $ionicLoading.hide();
+   }
+   var langData = LocalFactory.getData('countries');
+   if(!langData){
+     ApiFactory.getCountries().then(function(resp){
+       $scope.allCountries = resp.data;
+       LocalFactory.setData('countries', resp.data);
+       $ionicLoading.hide();
+     }, function(err){
+       console.log(err);
+     });
+   }else{
+     $scope.allCountries = langData;
+   }
+
+
   if(!$stateParams.hasOwnProperty("id") || !$stateParams.hasOwnProperty("name") || $stateParams.id == null ){
     //$state.go('towns');
     //return;
